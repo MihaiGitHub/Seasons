9a8fc02be5cce29eb8acb2450b390199fb1f8cca
Michael S <mihai.sanfran@gmail.com>
2019-05-02 21:59:52 -0700

    Refactor functional component to class based component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        // Success Callback
-        (position) => console.log(position),
-        // Failed Callback
-        (err) => console.log(err)
-    );
-    return <div>App Works!</div>;
-};
+class App extends React.Component {
+    // First function called when this class is created
+    constructor(props){
+        // Reference to the parents constructor function; React.Component constructor
+        super(props);
+
+        this.state = {
+            lat: null // state number initializing with null
+        };
+
+        // Gets executed once when this class is created
+        window.navigator.geolocation.getCurrentPosition(
+            // Success Callback
+            (position) => {
+                this.setState({
+                    lat: position.coords.latitude
+                });
+            },
+            // Failed Callback
+            (err) => console.log(err)
+        );
+    }
+
+    render(){
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
